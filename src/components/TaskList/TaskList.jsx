ed61c6ac67b2ff3f1d05d985af1a92363780c905
Giuseppe D'Orazio <doraziog@hotmail.com>
2025-09-09 12:32:40 -0400

    Added task list view, buttons for adding new tasks and changing view

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -5,14 +5,32 @@ function TaskList() {
 
   return (
     <>
+      <div class="task-list">
         {tasks.length === 0 ? (
             <p>No tasks are in your to-do list &#128077;</p>
-        ) : (
-            <p></p>
-        )
+          ) : (
+              tasks.map((task, index) => (
+                <li class="task-item" key={index}>
+                  <p>{task}</p>
+                </li>
+              ))
+          )
         }
+      </div>
+      <div class="buttons">
+        <button class="add-task-button" onClick={addTask}>Add Task</button>
+        <select class="select-task-button" name="Change View">
+          <option> View All Tasks</option>
+          <option> View Completed Tasks</option>
+          <option> View Active Tasks</option>
+        </select>
+      </div>
     </>
   )
+
+  function addTask() {
+    
+  }
 }
 
 export default TaskList
