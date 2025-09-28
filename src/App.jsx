f98eb846b56b5e20a401d72234c23aa442f09344
Giuseppe D'Orazio <doraziog@hotmail.com>
2025-09-28 19:56:23 -0400

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-
+import { useState } from 'react'
 import './App.css'
 import TaskList from './components/TaskList/TaskList.jsx'
+import AddTask from './components/TaskList/AddTask.jsx'
+
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [showForm, setShowForm] = useState(false);
+  const [filter, setFilter] = useState('all');
 
   return (
     <>
-    <header>
-        My To Do List
-    </header>
+      <header>
+          My To Do List
+      </header>
 
-    <main>
-      <TaskList />
-    </main>
+      <main>
+        {showForm ? (
+          <AddTask setTasks={setTasks} tasks={tasks} setShowForm={setShowForm} />
+        ) : (
+          <>
+            <TaskList tasks={tasks} setTasks={setTasks} filter={filter} />
+            <div>
+              <button className="click-button" onClick={(e) => setShowForm(true)}>Add Task</button>
+              <select value={filter} id="select-task" className="select-task-button" onChange={(e) => setFilter(e.target.value)}>
+                <option value="all"> View All Tasks</option>
+                <option value="completed"> View Completed Tasks</option>
+                <option value="active"> View Active Tasks</option>
+              </select>
+            </div>
+          </>
+        )}
+      </main>
     </>
   )
 }
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,43 +1,13 @@
-import { useState } from 'react'
-import AddTask from './AddTask.jsx'
+import { BsThreeDots } from "react-icons/bs";
 
+function TaskList({tasks, setTasks, filter}) {
 
-function TaskList() {
-  const [tasks, setTasks] = useState([])
-  const [filter, setFilter] = useState('all');
-  const [showForm, setShowForm] = useState(false);
+  if (tasks.length === 0) {
+    console.log("No tasks found");
+    return <p>No tasks are in your to-do list &#128077;</p>
+  }
   
-  return (
-    <>
-      {showForm ? (
-        <AddTask setTasks={setTasks} tasks={tasks} setShowForm={setShowForm} />
-      ) : (
-        <>
-              {tasks.length === 0 ? (
-                <p>No tasks are in your to-do list &#128077;</p>
-              ) : (
-              <Filter filter={filter} tasks={tasks} />
-              )}
-            <div>
-                <button className="click-button" onClick={addTask}>Add Task</button>
-                <select id="select-task" className="select-task-button" onChange={handleFilterChange}>
-                  <option value="all"> View All Tasks</option>
-                  <option value="completed"> View Completed Tasks</option>
-                  <option value="active"> View Active Tasks</option>
-                </select>
-            </div>
-        </>
-      )}
-    </>
-  )
-  function addTask() {
-    setShowForm(true);
-  }
-
-  function handleFilterChange() {
-    setFilter(document.getElementById("select-task").value);
-  }
-  function Filter({filter}) {
+  else {
     let filteredTasks = [...tasks];
     if(filter === 'active') {
       filteredTasks = filteredTasks.filter(task => !task.taskCompleted);
@@ -60,23 +30,24 @@ function TaskList() {
         {filteredTasks.map((task) => (
           <li key={task.taskID}>
             <div className="task-item" >
-              <input type="checkbox" checked={task.taskCompleted} onChange={() => toggleTaskCompletion(task.taskID, setTasks)}/>
+              <input className ="completion-checkbox" type="checkbox" checked={task.taskCompleted} onChange={() => toggleTaskCompletion(task.taskID, setTasks)}/>
               <div>
                 <p>Task: {task.taskName}</p>
               </div>
               <div>
                 <p>Due Date: {task.taskDueDate}</p>
               </div>
+              <button className="task-options-button"><BsThreeDots /></button>
             </div>
           </li>
         ))}
       </ul>
     )
-    function toggleTaskCompletion(id) {
-      const filteredTasks = tasks.map(task => task.taskID === id ? {...task, taskCompleted: !task.taskCompleted} : task);
-      console.log("toggling:", id);
-      setTasks(filteredTasks);
-    }
+  }
+  function toggleTaskCompletion(id) {
+    const filteredTasks = tasks.map(task => task.taskID === id ? {...task, taskCompleted: !task.taskCompleted} : task);
+    console.log("toggling:", id);
+    setTasks(filteredTasks);
   }
 }
 export default TaskList
